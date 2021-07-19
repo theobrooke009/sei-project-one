c47e38e7db66a7744e70c7b7df2a0d6baeac294b
Theo Brooke <theobrooke009@gmail.com>
2021-07-19 18:55:09 +0100

    nuked from orbit, start again - grid works

--- scripts/app.js
+++ scripts/app.js
@@ -1,325 +1,47 @@
 // Elements
 const grid = document.querySelector('.grid')
-// const weapon = document.querySelector('.weapon')
+// Grab the grid in the DOM
+
 const enemies = document.querySelectorAll('.enemy')
+// Grab the enemy class in the DOM
+
 const start = document.querySelector('.start')
+// Grab the HTML start button class in the dom
+
+
+
+
+// Arrays
 const cells = []
+// the blank array which the grid divs are pushed into
+
 let enemyArray = [0, 1, 2, 3, 4, 5, 6]
+// this represents the starting position of every enemy on the board
 
+// Grid & grid variables
 
-// Grid variables
-
-let width = 13
-const cellCount = width * width
-
-// Game Variables
-
-let timer = 0
-const playerClass = 'player'
-const weaponClass = 'weapon'
-const leadEnemy = 'leadEnemy'
-let enemy = 'enemy'
-const missileClass = 'missile'
-
-let playerPosition = parseFloat(Math.floor(cellCount - (width / 2)))
-let leadEnemyPosition = enemyArray[6]
-let enemyTwo = enemyArray[(leadEnemyPosition - 1)]
-let enemyThree = enemyArray[(leadEnemyPosition - 2)]
-let enemyFour = enemyArray[(leadEnemyPosition - 3)]
-let enemyFive = enemyArray[(leadEnemyPosition - 4)]
-let enemySix = enemyArray[(leadEnemyPosition - 5)]
-let enemySeven = enemyArray[(leadEnemyPosition - 6)]
-let shotInProgress = false
-let missilePoint = 9
-let row = 1
-
-// Functions
-
-//player functions
-
-
-function addPlayer() {
-  cells[playerPosition].classList.add(playerClass)
-}
-
-function removePlayer() {
-  cells[playerPosition].classList.remove(playerClass)
-}
-
-//weapon functions
-
-function weaponClassNuke() {
-  cells.forEach(cell => cell.classList.remove(weaponClass))
-}
-
-// function addWeapon() {
-//   cells[weaponPosition].classList.add(weaponClass)
-// }
-
-function missleClassAdd() {
-  cells[missilePoint].classList.add(missileClass)
-}
-
-function missleClassRemove() {
-  cells.forEach(cell => cell.classList.remove(missileClass))
-}
-
-//enemy functions
-
-
-// this changes the array just fine but I cant work out how to access the elements and apply them to enemy positions
-
-
-
-function addLeadEnemy() {
-  cells[leadEnemyPosition].classList.add('leadEnemy')  
-  cells[enemyTwo].classList.add('enemy')
-  cells[enemyThree].classList.add('enemy')
-  cells[enemyFour].classList.add('enemy')
-  cells[enemyFive].classList.add('enemy')
-  cells[enemySix].classList.add('enemy')
-  cells[enemySeven].classList.add('enemy')
-}
-
-function removeLeadEnemy() {
-  cells[leadEnemyPosition].classList.remove('leadEnemy')
-  cells[enemyTwo].classList.remove('enemy')
-  cells[enemyThree].classList.remove('enemy')
-  cells[enemyFour].classList.remove('enemy')
-  cells[enemyFive].classList.remove('enemy')
-  cells[enemySix].classList.remove('enemy')
-  cells[enemySeven].classList.remove('enemy')
-}
-
-
-
-  function moveEnemies() {
-    timer = setInterval(() => {
-  
-          removeLeadEnemy()
-          leadEnemyPosition++
-          enemyTwo++
-          enemyThree++
-          enemyFour++
-          enemyFive++
-          enemySix++
-          enemySeven++
-          addLeadEnemy()
-  
-  
-  
-          console.log(leadEnemyPosition)
-    }, 500
-    )
-  }
-  // const x = leadEnemyPosition % width
-  // let row = 1
-  // let isPathClear = true
-  
-  // setInterval(() => {
-  //   // function moveRight() {
-    
-  //   while (isPathClear) {
-      
-  //     console.log(leadEnemyPosition)
-  //     removeLeadEnemy()
-    
-  
-  //     enemyArray.map(mapEnemy => {
-  //       mapEnemy = mapEnemy + 1
-        
-  //       console.log(mapEnemy)
-  //       cells[mapEnemy].classList.add('enemy')
-  //       cells[mapEnemy].classList.add('leadEnemy')
-  //     })
-  //     console.log(enemyArray)
-  //     if (x >= (row * width) - 1) {
-        
-  //       isPathClear = false
-  //       return
-  //     }
-  //     return
-  //   } 
-        
-  //   // }
-  
-
-
-  // }, 500) 
-  //  function moveRight() {
-    
-  //   while (isPathClear) {
-  //     console.log('here')
-  //     removeLeadEnemy()
-  //     console.log('here')
-  //     enemyArray.map(mapEnemy => {
-  //       console.log('here')
-  //       mapEnemy += 1
-  //       console.log(mapEnemy)
-  //       cells[mapEnemy].classList.add(enemy)
-  //       cells[mapEnemy].classList.add(leadEnemy)
-  //     })
-  //     if (x >= (row * width) - 1) {
-  //       console.log('here')
-  //       isPathClear = false
-  //       return
-  //     }
-  //     return
-  //   } 
-      
-  // }
-
-  // moveRight()
-    
-
-  // }
-  // const x = leadEnemyPosition % width
-  // const y = Math.floor(leadEnemyPosition / width)
-  // if (y >= 0) {
-
-  //   if (leadEnemyPosition < cellCount - width){
-  //     removeLeadEnemy()
-  //     leadEnemyPosition++
-  //     enemyTwo++
-  //     enemyThree++
-  //     enemyFour++
-  //     enemyFive++
-  //     enemySix++
-  //     enemySeven++
-  //     addLeadEnemy()
-      
-
-      // } else if (width !== 0 && x === width - 1){
-      //   row += 1
-      //   removeLeadEnemy()
-      //   leadEnemyPosition += width
-      //   enemyTwo += width
-      //   enemyThree += width
-      //   enemyFour += width
-      //   enemyFive += width
-      //   enemySix += width
-      //   enemySeven += width
-      //   leadEnemyPosition--
-      //   width = 0
-      //   addLeadEnemy()
-      //   console.log(row)
-        
-
-      // } else if (width === 0 && x > 0 && leadEnemyPosition < cellCount - width) {
-      //   removeLeadEnemy()
-      //   leadEnemyPosition--
-      //   enemyTwo--
-      //   enemyThree--
-      //   enemyFour--
-      //   enemyFive--
-      //   enemySix--
-      //   enemySeven--
-      //   addLeadEnemy()
-      //   width = 13
-      // }
-   
-    }
-  }
-}
-
-//general game functions
-
+const gridWidth = 13
+const totalNumberOfGridCells = gridWidth * gridWidth
 
 function buildGrid() {
-  for (let i = 0; i < cellCount; i++) {
+  for (let i = 0; i < totalNumberOfGridCells; i++) {
     const newCell = document.createElement('div')
     newCell.textContent = i
     grid.appendChild(newCell)
     cells.push(newCell)
   }
-  addPlayer()
-  addLeadEnemy()
 }
-
+// this creates divs and pushes them into the cells array, thus creating the board. Perameters are defined in CSS.
 
 buildGrid()
+// Function call to build the grid
 
+// Functions
 
-function xAxisMove(event) {
-  const x = playerPosition % width
+// Player Functions
 
-  removePlayer()
+// Weapon functions
 
-  switch (event.keyCode) {
-    case 39:
-      if (x < width - 1) {
-        playerPosition++
-      }
-      break
-    case 37:
-      if (x > 0) {
-        playerPosition--
-      }
-      break
-  }
-  
-  addPlayer()
-}
+// Enemy Functions
 
-function endShot() {
-  clearInterval(timer)
-  weaponClassNuke()
-  
-}
-
-
-function fireWeapon(event) {
-  let weaponPosition = playerPosition + width
-
-  function addWeapon() {
-    cells[weaponPosition].classList.add(weaponClass)
-  }
-  
-  timer = setInterval(() => {
-  
-    const y = Math.floor(playerPosition / width)
-  
-    switch (event.keyCode) {
-      case 69:
-        if (weaponPosition >= y + 1) {
-            
-          weaponClassNuke()
-          weaponPosition -= width
-          addWeapon(weaponPosition)
-          console.log(weaponPosition)
-            
-        } else {
-          clearInterval(timer)
-          weaponClassNuke()
-          return
-        }
-        break      
-    } 
-  }) 
-}
-
-
-function enemyBomb() {
-  timer = setInterval(() => {
-    const y = Math.floor(playerPosition / width)
-    
-    if (leadEnemyPosition < cellCount - (width * 2)) {
-    //   let rNJesus = Math.floor((Math.random() * cellCount) + 1)
-      if (cells[missilePoint].classList.contains(enemy)){
-        missleClassRemove()
-        missilePoint += width
-        missleClassAdd()
-        console.log('fire bomb')
-      }
-    }
-  }, 1000
-  )
-}
-
-
-
-
-window.addEventListener('keyup', xAxisMove)
-window.addEventListener('keyup', fireWeapon)
-start.addEventListener('click', moveEnemies)
-start.addEventListener('click', enemyBomb)
+// Event Listeners
